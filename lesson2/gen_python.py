9786cd2eef62864f2100dfa114d2aa78929b537a
Vadim Vedmedenko <vadim.vedmedenko@gmail.com>
2025-03-02 14:46:35 +0200

    Fix filesize and prints

--- lesson2/gen_python.py
+++ lesson2/gen_python.py
@@ -19,7 +19,7 @@ print(f'Stage 1 - Generating with random')
 start_time = time.time()
 print(f'Stage 1.1 - Generating with randbytes {time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(start_time))}')
 with open(rnd_python_random_bytes, 'wb') as f:
-    for _ in range(2000000):
+    for _ in range(1200000):
         f.write(random.randbytes(1024)) 
 end_time = time.time()
 print(f'Execution time for randbytes: {end_time - start_time:.2f} seconds')
@@ -27,16 +27,16 @@ print(f'Execution time for randbytes: {end_time - start_time:.2f} seconds')
 start_time = time.time()
 print(f'Stage 1.2 - Generating with randint {time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(start_time))}')
 with open(rnd_python_random_int, 'wb') as f:
-    for _ in range(2000000000):
+    for _ in range(1200000000):
         num = random.randint(0, 255)
         f.write(bytes([num])) 
 end_time = time.time()
-print(f'Execution time for randbytes: {end_time - start_time:.2f} seconds')
+print(f'Execution time for randint: {end_time - start_time:.2f} seconds')
 
 start_time = time.time()
 print(f'Stage 2 - Generating with secrets {time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(start_time))}')
 with open(rnd_python_secrets, 'wb') as f:
-    for _ in range(2000000000):
+    for _ in range(1200000000):
         num = secrets.randbits(8)
         f.write(bytes([num])) 
 end_time = time.time()
@@ -44,6 +44,6 @@ print(f'Execution time for secrets: {end_time - start_time:.2f} seconds')
 
 start_time = time.time()
 print(f'Stage 3 - Generating with OpenSSL {time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(start_time))}')
-subprocess.run(["openssl", "rand", "-out", rnd_openssl, "1000000000"], check=True)
+subprocess.run(["openssl", "rand", "-out", rnd_openssl, "1200000000"], check=True)
 end_time = time.time()
 print(f'Execution time for OpenSSL: {end_time - start_time:.2f} seconds')
